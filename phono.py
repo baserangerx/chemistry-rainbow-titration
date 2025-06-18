9541c02c68efcace92890a967194986ae784dceb
baserangerx <baserangerx@gmail.com>
2025-06-18 15:11:27 -0700

    Final

--- phono.py
+++ phono.py
@@ -3,6 +3,7 @@ import struct
 import math
 import xml.etree.ElementTree as ET
 
+# Converts the data into a sin wave and stores it in an array
 def generate_sine_wave(frequency, duration, sample_rate=44100):
     num_samples = int(duration * sample_rate)
     amplitude = 2**15 - 1  # Max amplitude for 16-bit audio
@@ -10,6 +11,7 @@ def generate_sine_wave(frequency, duration, sample_rate=44100):
 
     for i in range(num_samples):
         #sample = amplitude * math.sin(2 * math.pi * frequency * i/sample_rate)
+
         sample = amplitude * math.sin(2 * math.pi * frequency * (i/sample_rate + getArea(i/sample_rate*divider)/14/divider))
         wave_data.append(int(sample))
         if (i % sample_rate) == 0:
@@ -17,23 +19,29 @@ def generate_sine_wave(frequency, duration, sample_rate=44100):
 
     return wave_data
 
-def save_wav(filename, data, sample_rate=44100):
+# Writes the sin wave array to a .wav file
+def save_wav(filename, _data, sample_rate=44100):
     with wave.open('output/'+filename, 'wb') as writefile:
         writefile.setnchannels(1)  # Mono audio
         writefile.setsampwidth(2)  # 16-bit audio
         writefile.setframerate(sample_rate)
-        for sample in data:
+        for sample in _data:
             writefile.writeframes(struct.pack('<h', sample)) # '<h' for 16-bit little endian
 
+# Returns the linearly interpolated value of Y
 def interpolate(x):
-    sum = 0.00
+    '''
+        y = y_offset + (x)(slope)
+    '''
+    y = 0.00
     for i in range(len(data[0])):
         #print(i)
         if(data[0][i] < x <= data[0][i+1]):
-            sum = data[1][i] + (x-data[0][i])*(data[1][i+1]-data[1][i])/(data[0][i+1]-data[0][i])
+            y = data[1][i] + (x-data[0][i]) * (data[1][i+1]-data[1][i])/(data[0][i+1]-data[0][i])
             break
-    return sum
+    return y
 
+# Returns the area under the graph from 0 -> X
 def getArea(x):
     sum = 0.00
     for i in range(len(data[0])):
@@ -44,20 +52,7 @@ def getArea(x):
             break
     return sum
 
-
-frequency = 220  # Frequency in Hz
-#duration = 2 # Duration in seconds
-
-data = [[],[]]
-#data[0] -> time : X
-#data[1] -> pH : Y
-
-divider = 6
-duration = 0
-#time in seconds
-
-area = []
-
+# Main function - converts _data into an audible .wav
 def generate_file(file_name, _data):
     global data, area, duration
     data = _data
@@ -71,4 +66,21 @@ def generate_file(file_name, _data):
     save_wav(file_name+'.wav', sine_wave)
     print(file_name+'.wav complete')
 
-#print("file completed")
+
+frequency = 220  # Frequency in Hz
+#duration = 2 # Duration in seconds
+
+# Stores a set of data points collected from the dataset
+data = [[],[]]
+#data[0] -> time : X
+#data[1] -> pH : Y
+
+# Divides the horizonal unit, shortening the duration of the wav file
+#   (Example: 180s / 6 -> 30s of audio)
+divider = 6
+duration = 0
+
+# Used for getArea() optimizations
+area = []
+
+
